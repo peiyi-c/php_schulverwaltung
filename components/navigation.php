0047d2fc15c7c66e28fd230a26460fcb8f151f7e
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-03 09:38:14 +0200

    change nav link color

--- components/navigation.php
+++ components/navigation.php
@@ -1,18 +1,18 @@
      <nav>
        <ul class="nav nav-tabs card-header-tabs">
          <li class="nav-item">
-           <button class="nav-link active" data-type="dashboard">Dashboard</button>
+           <button class="nav-link text-secondary active" data-type="dashboard">Dashboard</button>
          </li>
          <li class="nav-item">
-           <button class="nav-link" data-type="lehrer">Lehrer</button>
-         </li>
-
-         <li class="nav-item">
-           <button class="nav-link" data-type="schueler">Schüler</button>
+           <button class="nav-link text-secondary" data-type="lehrer">Lehrer</button>
          </li>
 
          <li class="nav-item">
-           <button class="nav-link" data-type="kurs">Kurs</button>
+           <button class="nav-link text-secondary" data-type="schueler">Schüler</button>
+         </li>
+
+         <li class="nav-item">
+           <button class="nav-link text-secondary" data-type="kurs">Kurs</button>
          </li>
        </ul>
      </nav>
