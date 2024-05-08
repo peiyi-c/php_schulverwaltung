0245ab1a663e79c1ea6e7378338f4a0066527cae
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-08 18:29:17 +0200

    correct search query

--- search.php
+++ search.php
@@ -23,29 +23,45 @@ $db = connect();
     <h1 class="my-5 text-center ">Schulverwaltung - Suchergebnis</h1>
     <div class="card text-center">
       <div class="card-header">
-
+        <div class="d-flex w-100 justify-content-start">
+          <!-- back to index.php -->
+          <button class="btn btn-primary"> <a href="./index.php" class=" text-decoration-none text-white-50">Zurück</a></button>
+        </div>
       </div>
       <div class="card-body w-75 mx-auto">
         <!-- search result -->
         <?php
         if (isset($_POST["search-schueler"])) {
+
           $vorname = $_POST["schueler-vorname-search"];
           $nachname = $_POST["schueler-nachname-search"];
-          $geburtsdatum = $_POST["schueler-geburtsdatum-search"];
+          $age = $_POST["schueler-geburtsdatum-search"];
           $klasse = $_POST["schueler-klasse-search"];
 
           try {
-            $query = "SELECT * FROM schueler WHERE Vorname LIKE :vorname OR Nachname LIKE :nachname OR Geburtsdatum LIKE :geburtsdatum OR Klasse LIKE :klasse";
-            if ($_POST["schueler-search-exact"] == 'exact') {
-              $query = "SELECT * FROM schueler WHERE Vorname LIKE :vorname AND Nachname LIKE :nachname AND Geburtsdatum LIKE :geburtsdatum AND Klasse LIKE :klasse";
+            $searchIdx = ['vorname', 'nachname', 'age', 'klasse'];
+            $searchInfo = array($vorname, $nachname, $age, $klasse);
+            $searchQuery = ['(Vorname LIKE :vorname)', '(Nachname LIKE :nachname)', '(ADDDATE(Geburtsdatum, INTERVAL :age YEAR) < CURRENT_DATE())', '(Klasse LIKE :klasse)'];
+
+            $searchExe = [];
+            $query = "SELECT * FROM schueler WHERE ";
+
+            foreach (array_values($searchInfo) as $i => $search) {
+              if ($search) {
+                $query .= $searchQuery[$i] . ' OR ';
+                $searchExe[$searchIdx[$i]] = $searchInfo[$i];
+              }
             }
+            // remove redundant 'OR '
+            $query = substr($query, 0, -3);
+
             $statement = $db->prepare($query);
-            $statement->execute(['vorname' => $vorname, 'nachname' => $nachname, 'geburtsdatum' =>  $geburtsdatum, 'klasse' => $klasse]);
+            $statement->execute($searchExe);
             $schuelers = $statement->fetchAll(PDO::FETCH_ASSOC);
 
             if ($schuelers) {
               echo
-              "<h3>Schüler</h3><table class='table'>
+              "<h3>Schüler</h3><table class='table mb-5'>
             <tr>
               <th>Vorname</th>
               <th>Nachname</th>
