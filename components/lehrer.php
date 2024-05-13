8884418d7ca4b8aa1be85d6a85979d154159ae04
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-13 11:41:46 +0200

    add Lehrer & Kurse table

--- components/lehrer.php
+++ components/lehrer.php
@@ -1,11 +1,12 @@
   <section id="lehrer" class="d-none container-fluid">
-    <h4 class="card-title">Lehrer-Liste</h4>
-    <?php
-    $statement = $db->query("SELECT * FROM lehrer");
-    $lehrer = $statement->fetchAll(PDO::FETCH_ASSOC);
+    <div class="">
+      <h4 class="card-title">Lehrer-Liste</h4>
+      <?php
+      $statement = $db->query("SELECT * FROM lehrer");
+      $lehrer = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    if ($lehrer) {
-      echo "<table class='table table-sm table-striped'>
+      if ($lehrer) {
+        echo "<table class='table table-sm table-striped'>
             <tr>
               <th>Lehrer ID</th>  
               <th>Vorname</th>
@@ -14,11 +15,34 @@
               <th>Geburtsdatum</th>
             
             </tr>";
-      foreach ($lehrer as $result) {
-        echo "<tr><td>"  . $result['Lehrer_ID'] . "</td><td>" . $result['Vorname'] . "</td><td>" . $result['Nachname'] . "</td><td>" . $result['E-Mail'] . "</td><td>" . $result['Geburtsdatum'] . '</td></tr>';
-      }
-      echo "</table>";
-    } else {
-      echo "<span>Keinen Lehrer gefunden</span>";
-    } ?>
+        foreach ($lehrer as $result) {
+          echo "<tr><td>"  . $result['Lehrer_ID'] . "</td><td>" . $result['Vorname'] . "</td><td>" . $result['Nachname'] . "</td><td>" . $result['E-Mail'] . "</td><td>" . $result['Geburtsdatum'] . '</td></tr>';
+        }
+        echo "</table>";
+      } else {
+        echo "<span>Keinen Lehrer gefunden</span>";
+      } ?>
+    </div>
+    <div class="mt-5">
+      <h4 class="card-title">Lehrer & Kurse</h4>
+      <?php
+      $statement = $db->query("SELECT * FROM lehrer LEFT JOIN kurs ON lehrer.Lehrer_ID = kurs.Lehrer_ID ORDER BY Vorname");
+      $lehrer = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+      if ($lehrer) {
+        echo "<table class='table table-sm container-sm '>
+            <tr>
+              <th>Name</th>
+              <th>Kurs</th>
+              <th>Kategorie</th>
+            </tr>";
+        foreach ($lehrer as $result) {
+          echo "<tr><td>"  . $result['Vorname'] .  ' ' .  $result['Nachname'] . "</td><td>" . $result['Titel'] . "</td><td>" . $result['Kategorie'] . '</td></tr>';
+        }
+        echo "</table>";
+      } else {
+        echo "<span>Keinen Lehrer gefunden</span>";
+      } ?>
+    </div>
+
   </section>
